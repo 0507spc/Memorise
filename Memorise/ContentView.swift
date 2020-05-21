81893b8e3cb2359180a44e5152b7be5fa1d9aa01
Steve Crane <0507spc@gmail.com>
2020-05-21 22:56:23 +0100

    Initial Commit

---
+++ Memorise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorise
+//
+//  Created by 0507 on 21/05/2020.
+//  Copyright © 2020 0507spc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
